9d3081e280a457ddb46f595f4df91808a884d74c
Noodleman <sherwoodwt@gmail.com>
2021-01-14 02:06:28 -0800

    Render sphere

---
+++ SpaCeGaMe2/Assets/PlanetGeneration/PlanetRenderer.cs
@@ -0,0 +1,40 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+
+namespace PlanetGeneration
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class PlanetRenderer : MonoBehaviour
+    {
+        public PlanetFaces planetFaces;
+
+        void Start()
+        {
+            planetFaces.Subscribe(name, OnRefresh);
+        }
+
+        void OnRefresh(Face[] faces)
+        {
+            var mesh = GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+            var combineInstances = new CombineInstance[faces.Length];
+            for (int i = 0; i < faces.Length; i++)
+            {
+                var f = faces[i];
+                float radius = f.width/2f;
+                var t = new GameObject().transform; // TODO: check for leaks
+                t.position = f.direction * radius;
+                t.forward = f.direction;
+                t.LookAt(f.direction);
+                combineInstances[i] = new CombineInstance
+                {
+                    mesh = new Mesh { vertices = f.vertices, triangles = f.triangles },
+                    transform = t.localToWorldMatrix,
+                };
+                Destroy(t.gameObject);
+            }
+            mesh.CombineMeshes(combineInstances, true, true);
+            mesh.RecalculateNormals();
+        }
+    }
+}
--- SpaCeGaMe2/Assets/PlanetGeneration/TerrainGenerator.cs
+++ SpaCeGaMe2/Assets/PlanetGeneration/TerrainGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace PlanetGeneration
 {
+    [RequireComponent(typeof(MeshFilter))]
     public class TerrainGenerator : MonoBehaviour
     {
         [Range(1, 50)]
@@ -19,7 +20,6 @@ namespace PlanetGeneration
         public float sparcity = .5f;
 
         public Directions directions;
-        public PlanetFaces planetFaces;
 
         public NoiseLayer[] noiseLayers;
         float constDisplacementX;
@@ -29,9 +29,30 @@ namespace PlanetGeneration
         void Refresh()
         {
             var faces = GenerateFaces(width, definition);
-            SetNeighbors(faces);
-            GetNoisy(faces);
-            planetFaces.Refresh(faces);
+            var mesh = GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+
+            var combines = new CombineInstance[faces.Length];
+            for (int i = 0; i < combines.Length; i++)
+            {
+                var f = faces[i];
+                float radius = f.width/2f;
+                var t = new GameObject().transform;
+                t.position = (f.direction * radius) + transform.position;
+                t.LookAt(f.direction + t.position);
+                t.RotateAround(t.position, t.right, 90);
+                combines[i] = new CombineInstance
+                {
+                    mesh = new Mesh { vertices = f.vertices, triangles = f.triangles },
+                    transform = t.localToWorldMatrix,
+                };
+                Destroy(t.gameObject);
+            }
+            mesh.CombineMeshes(combines, true, true);
+
+            mesh.vertices = MakeSpherical(mesh.vertices);
+            mesh.vertices = GetNoisy(mesh.vertices);
+            mesh.RecalculateNormals();
         }
 
         void Start()
@@ -42,7 +63,7 @@ namespace PlanetGeneration
             Refresh();
         }
 
-        void OnValidate()
+        void Update()
         {
             if (Application.isPlaying)
                 Refresh();
@@ -52,7 +73,6 @@ namespace PlanetGeneration
 
         Face[] GenerateFaces(int width, int definition)
         {
-            var pos = transform.position;
             var faces = new Face[directions.directions.Length];
             for (int i = 0; i < faces.Length; i++)
             {
@@ -68,57 +88,35 @@ namespace PlanetGeneration
             return faces;
         }
 
-        void SetNeighbors(Face[] faces)
+        Vector3[] GetNoisy(Vector3[] vertices)
         {
-            for (int i = 0; i < faces.Length; i++)
+            float maxNoise = 0;
+            float minNoise = float.MaxValue;
+            for (int i = 0; i < vertices.Length; i++)
             {
-                var cur = faces[i];
-                var neighbors = new List<Face>();
-                for (int j = 0; j < faces.Length; j++)
-                {
-                    if (i == j)
-                        continue;
-
-                    var neighbor = faces[j];
-                    if (i != j && neighbor.direction + cur.direction != Vector3.zero)
-                        neighbors.Add(neighbor);
-                }
-                cur.neighbors = neighbors.ToArray();
-
-                faces[i] = cur;
+                var loc = vertices[i] - transform.position;
+                var noise = MakeSomeNoise(loc);
+                if (noise > maxNoise)
+                    maxNoise = noise;
+                if (noise < minNoise)
+                    minNoise = noise;
+                vertices[i] = loc + (loc.normalized * noise);
             }
+            return vertices;
+
+            // float range = maxNoise-minNoise;
+            // for (int j = 0; j < vertices.Length; j++)
+            // {
+            //     float percentile = (vertices[j].y - minNoise) / range;
+            //     vertices[j].y *= percentile - sparcity;
+            //     // TODO: Fix sparcity normalization
+            //     // if (percentile < sparcity)
+            //     //     vertices[j].y *= sparcity - percentile;
+            // }
         }
 
-        void GetNoisy(Face[] faces)
-        {
-            for (int i = 0; i < faces.Length; i++)
-            {
-                var verts = faces[i].vertices;
-                float maxNoise = 0;
-                float minNoise = float.MaxValue;
-                for (int j = 0; j < verts.Length; j++)
-                {
-                    var noise = MakeSomeNoise(verts[j]);
-                    if (noise > maxNoise)
-                        maxNoise = noise;
-                    if (noise < minNoise)
-                        minNoise = noise;
-                    verts[j].z = noise;
-                }
-
-                // float range = maxNoise-minNoise;
-                // for (int j = 0; j < verts.Length; j++)
-                // {
-                //     float percentile = (verts[j].z - minNoise) / range;
-                //     verts[j].z *= percentile - sparcity;
-                //     // TODO: Fix sparcity normalization
-                //     // if (percentile < sparcity)
-                //     //     verts[j].z *= sparcity - percentile;
-                // }
-            }
-        }
-
-        float MakeSomeNoise(Vector2 value)
+        // TODO: Somethings wrong with lacunarity and persistence.
+        float MakeSomeNoise(Vector3 v)
         {
             float noise = 0;
             float freq = 1;
@@ -126,12 +124,40 @@ namespace PlanetGeneration
             for (int i = 0; i < noiseLayers.Length; i++)
             {
                 var layer = noiseLayers[i];
-                freq *= layer.frequency * (i+1) * lacunarity;
-                var flatNoise = Mathf.PerlinNoise(value.x * freq, value.y * freq);
-                amp *= (amp * (i+1)) / persistence;
-                noise += flatNoise * amp;
+                freq *= layer.frequency * Mathf.Pow(lacunarity, i);
+                float fx = freq * v.x;
+                float fy = freq * v.y;
+                float fz = freq * v.z;
+                var noises = new float[6]
+                {
+                    Mathf.PerlinNoise(fx, fy),
+                    Mathf.PerlinNoise(fx, fz),
+                    Mathf.PerlinNoise(fy, fz),
+                    Mathf.PerlinNoise(fy, fx),
+                    Mathf.PerlinNoise(fz, fx),
+                    Mathf.PerlinNoise(fz, fy),
+                };
+                float totNoise = 0;
+                for (int j = 0; j < noises.Length; j++)
+                {
+                    totNoise += noises[j];
+                }
+                float meanNoise = totNoise / noises.Length;
+                amp *= layer.intensity * Mathf.Pow(persistence, i);
+                noise += meanNoise * amp;
             }
             return noise;
+        }
+
+        Vector3[] MakeSpherical(Vector3[] vertices)
+        {
+            float radiusLength = width / 2f;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                var rad = vertices[i] - transform.position;
+                vertices[i] = rad.normalized * radiusLength;
+            }
+            return vertices;
         }
     }
 }
